0a49ccc362475ead2a8f2a7db03e975697ab1b31
eworshiper <76649204+eworshiper@users.noreply.github.com>
2021-11-22 23:49:42 +0700

    Delete MODUL 3 EVENETUS directory

--- MODUL 3 EVENETUS/method/Even_connect.php
+++
@@ -1,2 +0,0 @@
-<?php
-    $koneksi = mysqli_connect("localhost:3307","root","","modul3");
--- MODUL 3 EVENETUS/method/Even_delete.php
+++
@@ -1,8 +0,0 @@
-<?php
-    include('connect.php');
-
-        $id = $_GET['id_buku'];
-
-        $query = "DELETE FROM buku_table WHERE id_buku='$id'";
-        $delete = mysqli_query($konek, $query);
-?>
